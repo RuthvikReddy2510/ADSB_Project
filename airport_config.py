6d448fcdb1c78c7404bc71aaf011106605a8d9a2
RuthvikReddy2510 <ruthvikkawkuntla@gmail.com>
2025-07-21 10:40:52 -0700

    Bengaluru Airport addition

--- airport_config.py
+++ airport_config.py
@@ -6,6 +6,7 @@ AIRPORTS = {
     "SYD": {"name": "Sydney", "lat": -33.9399, "lon": 151.1753},
     "SIN": {"name": "Singapore Changi", "lat": 1.3644, "lon": 103.9915},
     "DEL": {"name": "Delhi IGI", "lat": 28.5562, "lon": 77.1000},
+    "BLR": {"name": "Bengaluru KIA", "lat": 13.1982, "lon": 77.7065},
     "ATL": {"name": "Atlanta Hartsfield", "lat": 33.6407, "lon": -84.4277},
     "SFO": {"name": "San Francisco International", "lat": 37.6213, "lon": -122.3790},
     "JFK": {"name": "New York JFK", "lat": 40.6413, "lon": -73.7781},
