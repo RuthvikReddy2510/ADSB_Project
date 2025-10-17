5ab303999e3efeea63a764ab50ded035c458569c
rkanicca <ruthvik.kawkuntla@aniccadata.com>
2025-10-17 16:59:42 -0700

    Limit change

--- threshold_config.py
+++ threshold_config.py
@@ -2,6 +2,6 @@
 
 THRESHOLDS = {
     "Air-Air": {"low": 8000, "medium": 4000, "high": 3000},
-    "Air-Ground": {"low": 1100, "medium": 950, "high": 800},
-    "Ground-Ground": {"low": 545, "medium": 524, "high": 515}
+    "Air-Ground": {"low": 5000, "medium": 3000, "high": 2000},
+    "Ground-Ground": {"low": 3000, "medium": 2000, "high": 1000}
 }
